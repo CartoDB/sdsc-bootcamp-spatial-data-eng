57927b360f6800018a8540c81386481e83112acd
cayetanobv <cayetanobv@gmail.com>
2023-09-05 20:30:13 +0200

    dbt - modified geom in models

--- dbt/sdsc_bootcamp/models/stg_data/public_transport.sql
+++ dbt/sdsc_bootcamp/models/stg_data/public_transport.sql
@@ -5,14 +5,15 @@
 with raw_source as (
 
     select * from {{ source('sdsc_bootcamp_data', 'raw_public_transportation') }}
-    where geom_wkt is not null and geoid is not null
+    where geoid is not null
 
 )
 , filter_cols as (
 
     select
         cast(geoid as int64) as id
-        , geom_wkt
+        , cast(latitude as float64) as latitude
+        , cast(longitude as float64) as longitude
         , cast(do_date as timestamp) as version_date
         , amenity
         , public_transport
@@ -28,8 +29,9 @@ with raw_source as (
 
     select
         *
-        , st_geogfromtext(geom_wkt) as geom
+        , st_geogpoint(longitude, latitude) as geom
     from filter_cols
+    where latitude is not null or longitude is not null
 
 )
 
