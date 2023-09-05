2406c52db27db587a25d9493582aa4e3c83b4b0e
cayetanobv <cayetanobv@gmail.com>
2023-09-06 00:07:16 +0200

    updated SQL scripts

--- scripts/load_bq_data_to_gcs.sql
+++ scripts/load_bq_data_to_gcs.sql
@@ -2,8 +2,8 @@
 -- This Big Query script creates a table with all the public transport nodes in the USA
 -- and exports it to a CSV file in a GCS bucket.
 
-DROP TABLE IF EXISTS `carto-ps-bq-developers.cayetanobv.osm_public_transport_usa_for_export`;
-CREATE TABLE `carto-ps-bq-developers.cayetanobv.osm_public_transport_usa_for_export`
+DROP TABLE IF EXISTS `my-project.my-dataset.osm_public_transport_usa_for_export`;
+CREATE TABLE `my-project.my-dataset.osm_public_transport_usa_for_export`
 As
 SELECT g.geoid, ST_X(g.geom) longitude, ST_Y(g.geom) latitude,
     cast(p.do_date as string) do_date, p.version, p.amenity,
@@ -19,14 +19,14 @@ WHERE public_transport IN ('platform', 'station', 'stop_position')
 
 EXPORT DATA
   OPTIONS (
-    uri = 'gs://carto-sdsc-bootcamp-data/osm_public_transport_usa/*.parquet',
+    uri = 'gs://my-gc-storage/osm_public_transport_usa/*.parquet',
     format = 'PARQUET',
     overwrite = true,
     compression = 'SNAPPY'
   )
 AS (
   SELECT *
-  FROM `carto-ps-bq-developers.cayetanobv.osm_public_transport_usa_for_export`
+  FROM `my-project.my-dataset.osm_public_transport_usa_for_export`
 );
 
-DROP TABLE IF EXISTS `carto-ps-bq-developers.cayetanobv.osm_public_transport_usa_for_export`;
+DROP TABLE IF EXISTS `my-project.my-dataset.osm_public_transport_usa_for_export`;
--- scripts/load_from_gcs_to_bq.sql
+++ scripts/load_from_gcs_to_bq.sql
@@ -1,5 +1,5 @@
 
-LOAD DATA OVERWRITE `carto-ps-bq-developers.sdsc_bootcamp_data.osm_public_transport_usa`
+LOAD DATA OVERWRITE `my-project.sdsc_bootcamp_data.osm_public_transport_usa`
 FROM FILES (
   format = 'PARQUET',
   uris = ['gs://carto-sdsc-bootcamp-data/osm_public_transport_usa/*.parquet']);
